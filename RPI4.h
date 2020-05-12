151ab1da16f86d1293340e394ff8d6a248357480
Tor Kaufmann Gjerde <34443415+TorKG1@users.noreply.github.com>
2020-05-13 01:19:19 +0200

    Update RPI4.h

--- RPI4.h
+++ RPI4.h
@@ -14,18 +14,15 @@
 
 #include <unistd.h>
 
-// Define which Raspberry Pi board are you using. Take care to have defined only one at time.
+// Define which Raspberry Pi board/chipset you are using  -> OASIS = RaspberryPi 4
 
 #define BCM2711_PERI_BASE       0xFE000000    // Peripheral base register for RPI4 
 #define GPIO_BASE               (BCM2711_PERI_BASE + 0x00200000)	// GPIO controller 
 
-
-
-
 #define PAGE_SIZE 		(4*1024)
 #define BLOCK_SIZE 		(4*1024)
 
-// IO Acces
+// Access 
 struct bcm2835_peripheral {
     unsigned long addr_p;
     int mem_fd;
@@ -33,22 +30,21 @@ struct bcm2835_peripheral {
     volatile unsigned int *addr;
 };
 
-extern struct bcm2835_peripheral gpio; 	// They have to be found somewhere, but can't be in the header
-extern struct bcm2835_peripheral bsc0;	// so use extern!!
+extern struct bcm2835_peripheral gpio; 	// 
+extern struct bcm2835_peripheral bsc0;	//
 
 
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
 #define INP_GPIO(g) 	*(gpio.addr + ((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) 	*(gpio.addr + ((g)/10)) |=  (1<<(((g)%10)*3))
-#define SET_GPIO_ALT(g,a) *(gpio.addr + (((g)/10))) |= (((a)<=3?(a) + 4:(a)==4?3:2)<<(((g)%10)*3))
 
-#define GPIO_SET 	*(gpio.addr + 7)  // sets   bits which are 1 ignores bits which are 0
-#define GPIO_CLR 	*(gpio.addr + 10) // clears bits which are 1 ignores bits which are 0
+#define GPIO_SET 	*(gpio.addr + 7)  // sets    bits which are 1 ignores bits which are 0
+#define GPIO_CLR 	*(gpio.addr + 10) // clears  bits which are 1 ignores bits which are 0
 
 #define GPIO_READ	        *(gpio.addr + 13) 
 #define GPIO_READ_PIN(g)	*(gpio.addr + 13) &= (1<<(g))
 
-// Function prototypes
+// Functions 
 int map_peripheral(struct bcm2835_peripheral *p);
 void unmap_peripheral(struct bcm2835_peripheral *p);
 
