819d88df1bfe1881b6a6cd188538ecf4622beba0
Tor Kaufmann Gjerde <34443415+TorKG1@users.noreply.github.com>
2020-05-12 19:22:07 +0200

    Update oasis_main_2.py

--- oasis_main_2.py
+++ oasis_main_2.py
@@ -1,61 +1,53 @@
-# ---------- For Plotting -----------
+# ---------- For Plotting ---------------------
 import math
 import matplotlib.pyplot as plt
-	
+import	
 # ---------- Other imports ------------------
 from subprocess import Popen, PIPE
 import time
-start = 1
+import RPi.GPIO as GPIO
 
+# --------------------- FUNCTIONS ------------------------------------------------------------------------------------------------------
 
-fft = True
-plot = False
+def get_ADC_data(): 
+    # This function returns a list with 1.7 million voltage readings taken at 1.7MHz sampling frequency.   
 
-while start == 1:
     counter = 0
-    Vref = 4.096
+    Vref = 4.096 # Reference voltage used in the ADC. 4.096 for internal voltage in ADS8422 
 
-    # start a subprocess: call the C code and let it run to do a parallel ADC read. 
+    #start a subprocess: call the C code and let it run to do a parallel ADC read. 
 
     c_process = Popen("./oasis_read_ADC_parallel", stdin = PIPE, stdout = PIPE)
-    outs, errs = c_process.communicate()
+    OUTS, errs = c_process.communicate()
 
-    #print(outs)
-
-    """---------------------------------------------------------------------------------------------------------------------------------
-    # Now the variable "outs" (bytes object) contains the states og the GPIO register after each ADC sample.
-    # Each integer represents a 32-bit register read. Read cycle = 3.33MHz, so sampling rate = 3.33MHz. 
-
-    NEXT TO DO: 
-     1. Sort all the integers into a list.
-    2. Convert integers to bit-string.
-    3. Manipulate string in order to extract the bits representing the GPIOs actually connected to the parallel ADC.
+    """
+    ---------------------------------------------------------------------------------------------------------------------------------
+    Now the variable "OUTS" (bytes object) contains a list with the states of the GPIO register after each ADC sample.
+    - Each entry in the list is an integer. 
+    - Each integer represents a 32-bit register read.
+    ------------------------------------------------------------------------------------------------------------------------------------
+    # The rest of this function converts the list of integers received from the C code into a list with voltages. 
+     ------------------------------------------------------------------------------------------------------------------------------------
+    1. Sort all the integers into a list.
+    2. Convert integers to 32-bit bit-string.
+    3. Manipulate string in order to extract the bits representing the GPIOs physically connected to the data lines of the parallel ADC.
     4. Sort the manipulated bits so that they have the right place in a 16-bit string. 
-    5. Convert the 16-bit ADC string to voltage reading - remember 16-bit value is two's complement.   
-
-    FURTHER ON:
-    1. Now we can plot data.
-    2. Signal processing.
-    3. FFT and so on! 
+    5. Convert the 16-bit string to voltage reading - remember 16-bit value is two's complement.   
     ----------------------------------------------------------------------------------------------------------------------------------
     """    
 
     data = outs.decode('UTF-8')               # Decode outs from byte-object and make a python string
-    data_list_string = data.split(',')        # Split the string at comma and put all entries into a python list 
-    number_of_samples = len(data_list_string)
+    data_list_string = data.split(',')        # Split the string at comma and put all entries into a new python list 
+    number_of_samples = len(data_list_string) # Get the total number of samples 
     
-    #print(data_list_string)
-    # Manipulate entries in the 'data' list to extract actual voltage reading from ADC: 
+    data_list_int = list(map(int, data_list_string)) # Convert string-entries to integers 
 
-    data_list_int = list(map(int, data_list_string)) # convert string-entries to integers 
-
-    bit32_list_string = [0]*number_of_samples
-    for i in range(0,number_of_samples):     # Convert integers from data_list_int to bit-string of 32 bits
+    bit32_list_string = [0]*number_of_samples   # Preallocating list
+    for i in range(0,number_of_samples):        # Convert integers from data_list_int to bit-string of 32 bit length. 
         bit32_list_string[i] = ('{0:032b}'.format(data_list_int[i]))  
-    #print(bit32_list_string)
 
-    bit16_list_string = [0]*number_of_samples
-    for i in range(0,number_of_samples):    # Extract bit position for the 16 GPIOs of interest from 32-bit register string
+    bit16_list_string = [0]*number_of_samples   # Preallocating list 
+    for i in range(0,number_of_samples):        # Extract bit position for the 16 GPIOs of interest from 32-bit bit-string. 
         bits = bit32_list_string[i]      
         #                    ADC-bit = RaspberryPi            index = (31 - GPIO)
         bit0  = bits[17]   # bit 0 =  GPIO 14       bit32_list_string  index 17
@@ -75,11 +67,8 @@ while start == 1:
         bit14 = bits[18]   # bit 14 = GPIO 13       bit32_list_string  index 18 
         bit15 = bits[25]   # bit 15 = GPIO 6        bit32_list_string  index 25 
 
-        
-
         # Format 16-bit string: MSB -> LSB, Two's complement 
         bit16_list_string[i] = (bit15 + bit14 + bit13 + bit12 + bit11 + bit10 + bit9 + bit8 + bit7 + bit6 + bit5 + bit4 + bit3 + bit2 + bit1 + bit0)
-        #print(bit16_list_string[i])
     
     bit16_list_int = [0]*number_of_samples
     for i in range(0, number_of_samples):                   # Convert bit string back to integer 
@@ -87,36 +76,50 @@ while start == 1:
     
         if bit16_list_int[i] > 32767:                       # Convert from two's complement   2**15-1 = 32767
             bit16_list_int[i] = -65536 + bit16_list_int[i]
-            #bit16_list_int[i] = -(bit16_list_int[i] - 32768)   
-		
+
 
     ADC_list_voltage = [0]*number_of_samples                # Convert ADC bit-data to voltage reading
     for i in range(0, number_of_samples):
         ADC_list_voltage[i] = (Vref) * (bit16_list_int[i] / (2**15))
     
-    #time.sleep(1)
-    # The list: ADC_list_voltage now contains all samples represented as voltage  
-  
+    # The list: ADC_list_voltage now contains all samples presented as voltage.   
+    return ADC_list_voltage
 
-    '''
-    for i in range(0,5):
-        print(f'{(ADC_list_voltage[i]):04f}')
-    '''
-    # ----- Plotting routine:
+# ---------------------------------END FUNCTION --------------------------------------------------------------------------------
+
+
+
+#--------------------------------------SETUP ---------------------------------------------------
+ GPIO.setmode(GPIO.BCM)
+ GPIO.setup(1, GPIO.OUT) # GPIO 0 used for communication Raspberry --> MCU 
+ GPIO.setup(0, GPIO.IN)  # GPIO 0 used for communication MCU --> Raspberry
+
+
+# ----------------------------------- MAIN LOOP ------------------------------------------------
+'''
+ADC-PCB --> RaspberryPi
+CONVST = GPIO 4
+BUSY = GPIO 3
+RD = GPIO 17 
+CS = connect to LOW 
+RESET = connect to HIGH 
+PD2 = connect to HIGH 
+'''
+RUN = True 
+while RUN == True: 
+    GPIO.output(1,1) # Set pin 1 to HIGH in order to trigger MCU to start chirping. 
+
+    while GPIO.input(0): 
+        # Wait for MCU to pull it's interface wire low. LOW signals indicates chirping done.  
     
-    output_file = open("output name here.txt", "w+")
-    output_file.write(str(ADC_list_voltage))
-    output_file.close()     
+
+    # Sample returning echo signal. 1.7 million samples are taken with a sampling frequency of 1.7MHz 
+    # total sampling time equals 1 second. 
+
+    Voltage_list = get_ADC_data()
     
-
-    plt.style.use('ggplot')
-    sample_list = range(0,number_of_samples)
-    plt.plot(sample_list, ADC_list_voltage, color='red', linewidth=1)
-    plt.tight_layout()
-    plt.show()
-
-
-    start = 0  
-
+    GPIO.output(1,0) # Set pin 1 to LOW 
+     	
+    RUN = False 
 
 
