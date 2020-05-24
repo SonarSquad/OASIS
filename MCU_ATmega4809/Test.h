a97a92f91e0a399dcbd338f5e707069cc14da6d7
Tor Kaufmann Gjerde <34443415+TorKG1@users.noreply.github.com>
2020-05-24 14:55:13 +0200

    Add files via upload

---
+++ MCU_ATmega4809/Test.h
@@ -0,0 +1,16 @@
+﻿/*
+ * _500k.h
+ *
+ * Created: 05.03.2020 15:40:27
+ *  Author: Torbjørn Wilson
+ */ 
+
+
+#ifndef TEST_H_
+#define TEST_H_
+
+void chirp(void);
+
+
+
+#endif /* 500K_H_ */
---
+++ MCU_ATmega4809/main.c
@@ -0,0 +1,49 @@
+/*
+ * OASIS 
+ * Sine chirp generator
+ * Created: 05.03.2020 10:26:09
+ * Edited:  04.05:2020 10:13:37
+ */
+
+#ifndef F_CPU
+#define F_CPU 20000000
+#endif
+
+#define TR_Switch_TX_and_CPU_Sync	PORTD_OUTSET = 0x03
+#define TR_Switch_RX_and_CPU_Sync	PORTD_OUTCLR = 0x03
+
+#include <avr/io.h>
+#include "test.h"
+#include <avr/interrupt.h>
+
+void ClkSelect(void);
+
+int main(void)
+{
+	ClkSelect(); //Forces 20MHz system clock
+	PORTD_DIR		|= PIN0_bm |PIN1_bm;  //TX/RX and CPU_sync as OUTPUT 
+	PORTF_DIR		|= PIN4_bm |PIN5_bm;  //PWM pins as OUTPUT
+	PORTD.PIN0CTRL	|= PORT_ISC_RISING_gc;//Enable PORTD pin change interrupt 
+	sei();		//Enable global interrupt
+    while (1) 
+	{  
+		//Waits for CPU signal
+    }
+}
+
+ISR(PORTD_PORT_vect)
+{
+	cli();						//Disable global interrupt
+	PORTD_INTFLAGS = 0xFF;		//Clearing interrupt flags
+	TR_Switch_TX_and_CPU_Sync;  //Switches the transducer to transmit mode
+	chirp();				    //Down Chirp from 208kHz - 192 kHz
+	TR_Switch_RX_and_CPU_Sync;	//Switches the transducer to receive mode
+	sei();						//reenable global interrupts
+}
+
+void ClkSelect(void)
+{	//Forces 20MHz CPU clock frequency (disables prescaler)
+	_PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, 0<<CLKCTRL_PEN_bp); 
+}
+
+
